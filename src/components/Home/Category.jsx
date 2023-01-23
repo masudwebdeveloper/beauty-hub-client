e13f3d073561a15714d3d67fc6b0b4d8e4f2e813
masudrana5 <abdullainfo30@gmail.com>
2023-01-23 10:09:49 +0600

    categories section update

--- src/components/Home/Category.jsx
+++ src/components/Home/Category.jsx
@@ -5,10 +5,10 @@ const Category = ({ category }) => {
   const { img, name } = category;
   return (
     <Link>
-      <div className="relative group">
+      <div className="relative group overflow-hidden">
         <img className="rounded-t group-hover:scale-110 duration-500" src={img} alt="" />
-        <div className="absolute group-hover:scale-110 -bottom-10 bg-gradient-to-t from-gray-800 opacity-90 transition group-hover:-translate-y-10 origin-top duration-500 w-full h-[50%]"></div>
-        <h1 className="text-slate-50 absolute group-hover:scale-110 -bottom-7 group-hover:-translate-y-10 origin-top duration-500 left-0 text-center w-full">{name}</h1>
+        <div className="absolute group-hover:scale-110 bottom-0 bg-gradient-to-t from-gray-800 opacity-90 transition group-hover:-translate-y-0 origin-top duration-500 w-full h-[50%]"></div>
+        <h1 className="text-slate-50 absolute bottom-2 group-hover:scale-110  group-hover:-translate-y-5 origin-top duration-500 left-0 text-center w-full">{name}</h1>
       </div>
     </Link>
   );
